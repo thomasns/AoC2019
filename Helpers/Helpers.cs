8136127a0cf780d4c290fa325b813998587c8161
Thomasns <THOMASNS@ETSU.EDU>
2019-12-02 07:39:14 -0500

    Created a class to Handle the intcode computer incase it comes up again. Created an empty day 3 project.

---
+++ Helpers/Helpers.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace AoC2019
+{
+    class Helpers
+    {
+
+        public static void Test()
+        {
+            int a;
+        }
+        public static String readToBuffer(int day, string prefix = "") {
+            StreamReader sr = new StreamReader(@"C:\Users\thoma\Documents\AoC\input\day"+ prefix + day + @".txt");
+            string input = sr.ReadToEnd();
+            return input;
+        }
+
+        public static List<int> readToIntList(int day, char delim = '\n', string prefix = "")
+        {
+            String strIn = readToBuffer(day);
+            String[] parts = strIn.Trim().Split(delim);
+
+            List<int> lout = new List<int>();
+            foreach (string s in parts)
+                lout.Add(int.Parse(s));
+            return lout;
+
+        }
+
+        public static List<String> readToStringList(int day, char delim = '\n')
+        {
+            String strIn = readToBuffer(day);
+            String[] parts = strIn.Trim().Split(delim);
+
+            return parts.ToList<String>();
+
+
+        }
+
+
+
+
+
+    }
+}
---
+++ Helpers/Intcode.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2019
+{
+    class Intcode
+    {
+        private int day; //used to reset easily
+
+        //tracks the Intcode computers state
+        private List<int> mem;
+        private int PC;
+        private bool running;
+    
+        public Intcode(int noun, int verb, int day)
+        {
+            this.day = day;
+            mem = Helpers.readToIntList(day, ',',"i");
+            PC = 0;
+            mem[1] = noun;
+            mem[2] = verb;
+            running = false;
+        }
+
+
+        //runs until opcode 99 is encountered
+        public void run()
+        {
+            running = true;
+            while(running)
+            {
+                Step();
+            }
+        }
+
+        //Returns the value at postion 0
+        //Should probably throw an exception or something if the computer hasn't run
+        public int getOutput()
+        {
+            return mem[0];
+        }
+    
+        //Runs the computer for one instruction
+        private void Step()
+        {
+            int istr = mem[PC];
+            int op1 = mem[mem[PC + 1]];
+            int op2 = mem[mem[PC + 2]];
+            int sto = mem[PC + 3];
+
+            if (istr == 1)
+                mem[sto] = op1 + op2;
+            if (istr == 2)
+                mem[sto] = op1 * op2;
+            if(istr == 99)
+            {
+                running = false;
+            }
+
+            PC += 4;
+
+        }
+
+        //Resets the computer to its original memory state with a new noun and verb
+        public void reset(int noun, int verb)
+        {
+            mem = Helpers.readToIntList(day, ',',"i");
+            PC = 0;
+            mem[1] = noun;
+            mem[2] = verb;
+            running = false;
+        }
+
+
+
+
+    }
+}
